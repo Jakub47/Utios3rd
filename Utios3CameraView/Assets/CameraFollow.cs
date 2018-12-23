37256e59fb6d26a49739830c008a690ebb60c37a
Jakub47 <jakub7447@gmail.com>
2018-12-23 19:59:25 +0100

    Prototyping

---
+++ Utios3CameraView/Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour 
+{
+
+	private GameObject Player;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Player = GameObject.FindGameObjectWithTag ("Player");	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () 
+	{
+		transform.position = Player.transform.position;
+	}
+}
