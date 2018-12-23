2ffb386bac1b37d53da7da8a44d720df1f28be23
Jakub47 <jakub7447@gmail.com>
2018-12-23 20:39:02 +0100

    RayCast + Layer + LayerMask

---
+++ Utios3CameraView/Assets/Cursor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+	CameraRaycaster hit;
+
+	// Use this for initialization
+	void Start () 
+	{
+		hit = GetComponent<CameraRaycaster> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Debug.Log (hit.layerHit);	
+	}
+}
