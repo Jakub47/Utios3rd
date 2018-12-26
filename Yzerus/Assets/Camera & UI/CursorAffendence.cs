6598371184ffc6db39c6d0a50f4845412790a41c
Jakub47 <jakub7447@gmail.com>
2018-12-26 01:42:47 +0100

    Cursor changed based on layer + Cursor basic asset!

---
+++ Yzerus/Assets/Camera & UI/CursorAffendence.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffendence : MonoBehaviour
+{
+	[SerializeField] Texture2D AttackCursor;
+	[SerializeField] Texture2D WalkCursor;
+	[SerializeField] Texture2D UnknownCursor;
+	[SerializeField] Vector2 cursorHotspot = new Vector2 (96, 96);
+	CameraRaycaster hit;
+
+	// Use this for initialization
+	void Start () 
+	{
+		hit = GetComponent<CameraRaycaster> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		switch (hit.layerHit) 
+		{
+			case Layer.Enemy:
+			Cursor.SetCursor (AttackCursor,cursorHotspot, CursorMode.Auto);		
+			break;
+
+			case Layer.Walkable:
+			Cursor.SetCursor (WalkCursor,cursorHotspot, CursorMode.Auto);		
+			break;
+
+			default:
+			Cursor.SetCursor (UnknownCursor,cursorHotspot, CursorMode.Auto);	
+			return;
+		}
+	}
+}
