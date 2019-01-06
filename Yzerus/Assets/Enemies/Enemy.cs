2c5c07321780526f194df6fb846ae79391d3b0c5
Jakub47 <jakub7447@gmail.com>
2019-01-06 14:41:08 +0100

    Design stuff #1

---
+++ Yzerus/Assets/Enemies/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour 
+{
+	[SerializeField] float maxHealthPoints = 100f;
+
+	public float currentHealthPoints = 100f;
+	public float healthAsPercentage
+	{
+		get 
+		{
+			return currentHealthPoints / (float)maxHealthPoints;
+		}
+	}
+
+	[SerializeField] float maxManaPoints = 100f;
+
+	float currentManaPoints = 100f;
+	public float ManaAsPercentage
+	{
+		get 
+		{
+			return currentHealthPoints / (float)maxHealthPoints;
+		}
+	}
+
+	[SerializeField] float maxStaminaPoints = 100f;
+
+	float currentStaminaPoints = 100f;
+	public float StaminaAsPercentage
+	{
+		get 
+		{
+			return currentHealthPoints / (float)maxHealthPoints;
+		}
+	}
+
+}
